566b5124e5df4162875910e3b23ac3fc948d4357
Juliette Rapala <juliette@eventric.com>
2019-09-18 13:08:47 -0500

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
+const Logo = require('./images/logo.svg')
 import React, { Fragment } from 'react'
 import { render } from 'react-dom'
-// import Logo from './images/Logo.svg'
-const Logo = require('./images/logo.svg')
 import InputRow from './InputRow'
 import AlbumInfo from './AlbumInfo'
 
