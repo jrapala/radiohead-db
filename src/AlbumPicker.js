9bef392333b79bde6cde32058e89f2516c919f3b
Juliette Rapala <juliette@eventric.com>
2019-10-13 14:22:01 -0500

    Turn InputRows into SongPicker and AlbumPicker

---
+++ src/AlbumPicker.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AlbumPicker = () => {
+	return (
+		<div className="input-row">
+			<label htmlFor="album">View an Album:</label>
+			<div>
+				<select id="album"></select>
+			</div>
+		</div>
+	)
+}
+
+export default AlbumPicker
