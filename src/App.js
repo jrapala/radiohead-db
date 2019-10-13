3395fc870c47e8341e5df12ee24a7a8503cdd390
Juliette Rapala <juliette@eventric.com>
2019-10-13 15:36:45 -0500

    Add ApolloClient and wrap app in ApolloProvider

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React, { Fragment } from 'react'
 import { render } from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+
 import Logo from './images/logo.svg'
 import SongPicker from './SongPicker'
 import AlbumPicker from './AlbumPicker'
 import AlbumInfo from './AlbumInfo'
 
+const client = new ApolloClient({
+	uri: 'http://localhost:4000/',
+})
+
 const albums = []
 
 const App = () => {
 	return (
-		<Fragment>
-			<img src={Logo} alt="Radiohead Logo" />
-			<h2>Album Database</h2>
-			<SongPicker />
-			<AlbumPicker />
-			{albums.forEach(album => (
-				<AlbumInfo album={album} />
-			))}
-		</Fragment>
+		<ApolloProvider client={client}>
+			<Fragment>
+				<img src={Logo} alt="Radiohead Logo" />
+				<h2>Album Database</h2>
+				<SongPicker />
+				<AlbumPicker />
+				{albums.forEach(album => (
+					<AlbumInfo album={album} />
+				))}
+			</Fragment>
+		</ApolloProvider>
 	)
 }
 
