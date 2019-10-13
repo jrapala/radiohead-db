3ae54ea4ae8dbaba6118bb60cea6375bff5472a1
Juliette Rapala <juliette@eventric.com>
2019-10-13 16:07:44 -0500

    Populate AlbumPicker with options

--- src/AlbumPicker.js
+++ src/AlbumPicker.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import { gql } from 'apollo-boost'
+import { useQuery } from '@apollo/react-hooks'
+
+const GET_ALL_ALBUMS = gql`
+	{
+		allAlbums {
+			id
+			title
+		}
+	}
+`
 
 const AlbumPicker = () => {
+	const { loading, error, data } = useQuery(GET_ALL_ALBUMS)
+
+	if (loading) return <p>Loading...</p>
+	if (error) return <p>Error</p>
+
 	return (
 		<div className="input-row">
 			<label htmlFor="album">View an Album:</label>
 			<div>
-				<select id="album"></select>
+				<select id="album" defaultValue="">
+					<option value="disabled"></option>
+					{data.allAlbums.map(album => (
+						<option key={album.id} value={album.id}>
+							{album.title}
+						</option>
+					))}
+				</select>
 			</div>
 		</div>
 	)
