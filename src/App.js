71aaa238d245e5e30507b20d9bb43a4d79be8a4c
Juliette Rapala <juliette@eventric.com>
2019-10-13 17:59:26 -0500

    Move primary components to Main.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React, { Fragment } from 'react'
 import { render } from 'react-dom'
 import Logo from './images/logo.svg'
-import SongPicker from './SongPicker'
-import AlbumPicker from './AlbumPicker'
-import AlbumInfo from './AlbumInfo'
-
-const albums = []
+import Main from './Main'
 
 const App = () => {
 	return (
 		<Fragment>
 			<img src={Logo} alt="Radiohead Logo" />
 			<h2>Album Database</h2>
-			<SongPicker />
-			<AlbumPicker />
-			{albums.forEach(album => (
-				<AlbumInfo album={album} />
-			))}
+			<Main />
 		</Fragment>
 	)
 }
