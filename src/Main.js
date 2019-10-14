15588e381ce8ea64dd8419caa3ccf973a088b7c9
Juliette Rapala <juliette@eventric.com>
2019-10-13 19:16:12 -0500

    Add query for selected album

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,34 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
+
 import SongPicker from './SongPicker'
 import AlbumPicker from './AlbumPicker'
 import AlbumInfo from './AlbumInfo'
 
+const GET_ALBUM = gql`
+	query($id: ID!) {
+		findAlbumById(id: $id) {
+			title
+			releaseYear
+			songs {
+				songTitle
+			}
+		}
+	}
+`
+
 const albums = []
 
 const Main = () => {
+	const [album, selectAlbum] = useState()
+	const { loading, error, data } = useQuery(GET_ALBUM, {
+		variables: { id: album },
+		skip: !album,
+	})
+
 	const handleAlbumSelection = event => {
-		console.log(event.target.value)
+		selectAlbum(event.target.value)
 	}
 
 	return (
